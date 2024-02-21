4c026fce03ebe537fb00fe6322c33077e8a1c19c
perfect-credit <185212043+perfect-credit@users.noreply.github.com>
2024-02-20 23:05:02 -0600

    Cambios en el login 20/2/2024

--- login.php
+++ login.php
@@ -3,13 +3,38 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--- CDN Bootstrap--->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <!--- CSS--->
+    <link rel="stylesheet" href="css/login.css">
     <title>Portal Us</title>
 </head>
+
 <body>
-    <form action="">
-<input type="text">
-
-
-    </form>
+<div class="container login">
+<div class="card text-center">
+  <div class="card-header">
+  <img src="img/UTC_LOGO.png" class="img-fluid" alt="alternative" width="100px" height="100px">
+  </div>
+  <div class="card-body">
+  <form action="">
+    <div class="mb-3">
+     
+       <input type="number" class="form-control" id="matricula" placeholder="Matricula escolar">
+    </div>
+   <div class="mb-3">
+       <input type="password" class="form-control" id="psw" placeholder="Contraseña">
+    </div>
+  </form>
+  </div>
+  <div class="card-footer text-body-secondary">
+  Desarrollado por el grupo Dev 3CPS
+  <br><br>
+  Universidad Tecnológica de Coahuila © 2024
+  </div>
+  
+ 
+</div>
 </body>
 </html>
